a28977e901e792c4f044a253b28d1f88c85a75e3
Henrique Douglas <henriquedouglas2803@gmail.com>
2023-10-24 19:02:30 -0300

    adiciona projeto

---
+++ controllers/RaceController.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../utils.php';
+require_once '../models/Race.php';
+
+class RaceController
+{
+
+    public function createOne()
+    {
+        $body = getBody();
+
+        $name = sanitizeInput($body, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if (!$name) responseError('O nome é obrigatório', 400);
+
+        $race = new Race($name);
+
+        $race->insert();
+        
+       
+    }
+}
